dddae9d3d7ff5cdb99a777290a379a748a5e2e24
futursolo@gmail.com <futursolo@gmail.com>
2015-11-14 11:53:09 -0500

    FutureFinity 0.0.1 Release.

--- futurefinity/__init__.py
+++ futurefinity/__init__.py
@@ -14,6 +14,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-__version__ = ("0", "0", "1", "-1000")
+__version__ = ("0", "0", "1", "0")
 
-version = "0.0.1dev0"
+version = "0.0.1"
